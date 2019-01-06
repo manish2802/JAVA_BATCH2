3ad7e35fc1581210a5aaa57a6f3a815eb20f3dcc
manish2802 <manishverma.java@gmail.com>
2019-01-06 22:10:21 +0530

    CustomLinked List

---
+++ JAVA/COLLECTION/src/main/java/list/customlinkedlist/Operation.java
@@ -0,0 +1,13 @@
+package list.customlinkedlist;
+
+public class Operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CustomLinkedList customlinkedlist=new CustomLinkedList();
+		customlinkedlist.add("manish");
+		customlinkedlist.add("verma");
+		System.out.println(customlinkedlist);
+	}
+
+}
