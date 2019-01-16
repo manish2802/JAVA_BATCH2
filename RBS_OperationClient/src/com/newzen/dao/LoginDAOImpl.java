800de746c895dbeb2dfe35ab274b56ec237e9678
manish2802 <manishverma.java@gmail.com>
2019-01-16 20:59:02 +0530

    new

--- RBS_OperationClient/src/com/newzen/dao/LoginDAOImpl.java
+++ RBS_OperationClient/src/com/newzen/dao/LoginDAOImpl.java
@@ -1,5 +1,24 @@
 package com.newzen.dao;
 
-public class Test {
+public class LoginDAOImpl implements LoginDAO {
+
+	public static LoginDAO INSTANCE;
+
+	private LoginDAOImpl() {
+	}
+
+	public static synchronized LoginDAO getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new LoginDAOImpl();
+			return INSTANCE;
+		}
+		return INSTANCE;
+	}
+
+	@Override
+	public boolean login(String userName, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- RBS_OperationClient/src/com/newzen/service/LoginBOImpl.java
+++ RBS_OperationClient/src/com/newzen/service/LoginBOImpl.java
@@ -1,5 +1,24 @@
 package com.newzen.service;
 
-public class Test {
+public class LoginBOImpl implements LoginBO {
+
+	public static LoginBOImpl INSTANCE;
+
+	private LoginBOImpl() {
+	}
+
+	public static synchronized LoginBO getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new LoginBOImpl();
+			return INSTANCE;
+		}
+		return INSTANCE;
+	}
+
+	@Override
+	public boolean login(String userName, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
